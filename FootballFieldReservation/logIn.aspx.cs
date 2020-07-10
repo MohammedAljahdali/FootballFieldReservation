eedc20d883bd0889582861ba5a84c1013efa7bb4
MoahmmedAljahdali <mohammedkhalid4028@gmail.com>
2020-07-10 23:04:39 +0300

    delete admin.aspx

--- FootballFieldReservation/logIn.aspx.cs
+++ FootballFieldReservation/logIn.aspx.cs
@@ -50,16 +50,11 @@ namespace FootballFieldReservation
 
         protected void ForgotPassCode_Click(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-
-=======
             System.Diagnostics.Debug.WriteLine(File.OpenText(
                 (AppDomain.CurrentDomain.BaseDirectory.ToString())
                 .Substring(0,
                 AppDomain.CurrentDomain.BaseDirectory.ToString().Length -
                 "FootballFieldReservation".Length - 1) + "ignore/" + "conn.txt").ReadToEnd());
-         
->>>>>>> 4ec58859fa04ef038437f04d1c61fd502a575e02
             if (userNameTxt.Text.Equals(""))
             {
                 GlobalVar.showMessage("Enter your user name ", WarningType.Danger, Master);
